88e32cdde5263f50dd8d26a91f1b4371b7f06f47
Inna <inna.rekesh@gmail.com>
2020-10-02 00:32:13 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let min = array[0];
+    array.forEach(i => {
+        if (i < min) min = i;
+    })
+    return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let max = array[0];
+    array.forEach(i => {
+        if (i > max) max = i;
+    })
+    return max
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((acc, i) => {
+        return acc + i
+    }) / array.length
+
 }
